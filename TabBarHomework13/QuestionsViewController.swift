047947532f65edc4e226438823fe30406991feee
Vampirius1 <111462054+Vampirius1@users.noreply.github.com>
2023-03-25 11:22:15 +0300

    Create Views & Controllers clases

---
+++ TabBarHomework13/QuestionsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  QuestionsViewController.swift
+//  TabBarHomework13
+//
+//  Created by Владислав on 25.03.2023.
+//
+
+import UIKit
+
+class QuestionsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
